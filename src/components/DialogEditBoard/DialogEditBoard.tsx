3d8894df7ac83553ae3d1e9ded6411efe172f0a8
Bumble <bumble.sakh@gmail.com>
2022-11-15 21:30:28 +1100

    feat: add edit board dialog

---
+++ src/components/DialogEditBoard/DialogEditBoard.tsx
@@ -0,0 +1,98 @@
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import Stack from '@mui/joy/Stack';
+import Textarea from '@mui/joy/Textarea';
+import TextField from '@mui/joy/TextField';
+import Typography from '@mui/joy/Typography';
+
+import { useEffect } from 'react';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { isEditedFalse, setIsOpenedDialogEditBoard } from '../../store/slices/boards/boardsSlice';
+
+type FormType = {
+  title: string;
+  description: string;
+};
+
+const clearForm = {
+  title: '',
+  description: '',
+};
+
+export const DialogEditBoard = () => {
+  const { isEdited, isOpenedDialogEditBoard } = useAppSelector((state) => state.boards);
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+  const { control, handleSubmit, reset } = useForm<FormType>();
+
+  useEffect(() => {
+    if (isEdited) {
+      toast.success(t('boardEdited'));
+      dispatch(isEditedFalse());
+    }
+  }, [isEdited, dispatch, t]);
+
+  const onClose = () => {
+    reset(clearForm);
+    dispatch(setIsOpenedDialogEditBoard(false));
+  };
+
+  const onSubmit: SubmitHandler<FormType> = () => {
+    onClose();
+  };
+
+  return (
+    <Modal open={isOpenedDialogEditBoard} onClose={onClose}>
+      <ModalDialog
+        aria-labelledby="basic-modal-dialog-title"
+        aria-describedby="basic-modal-dialog-description"
+        sx={{
+          maxWidth: 500,
+          borderRadius: 'md',
+          p: 3,
+          boxShadow: 'lg',
+        }}
+      >
+        <Typography id="basic-modal-dialog-title" component="h2" level="inherit" fontSize="1.25em" mb="0.25em">
+          {t('editBoard')}
+        </Typography>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack spacing={2}>
+            <Controller
+              name="title"
+              control={control}
+              defaultValue={`title`}
+              rules={{ required: 'Field is require' }}
+              render={({ field }) => <TextField {...field} label={t('title')} autoFocus required />}
+            />
+
+            <Controller
+              name="description"
+              control={control}
+              defaultValue={`description`}
+              rules={{ required: 'Field is require' }}
+              render={({ field }) => {
+                return (
+                  <FormControl>
+                    <FormLabel required>{t('description')}</FormLabel>
+                    <Textarea {...field} minRows={3} required />
+                  </FormControl>
+                );
+              }}
+            />
+
+            <Button type="submit">{t('editBoard')}</Button>
+          </Stack>
+        </form>
+      </ModalDialog>
+    </Modal>
+  );
+};
