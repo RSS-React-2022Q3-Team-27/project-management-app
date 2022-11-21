c439055eb530e753be4d0d3074a80e8d4db4db38
Bumble <bumble.sakh@gmail.com>
2022-11-21 21:05:17 +1100

    feat: add redux rerender

--- src/components/Board/Columns/Columns.tsx
+++ src/components/Board/Columns/Columns.tsx
@@ -15,7 +15,7 @@ import {
   useGetColumnsInBoardQuery,
   useUpdateSetOfColumnsMutation,
 } from '../../../store/slices/board/boardApi';
-import { openAddColumnModal, setColumnsLength } from '../../../store/slices/board/boardSlice';
+import { openAddColumnModal, saveColumnTasks, setColumnsLength } from '../../../store/slices/board/boardSlice';
 import { TaskType } from '../../../store/slices/tasks/tasksApi';
 import { Column } from '../Column';
 
@@ -26,13 +26,13 @@ export const Columns = () => {
   const [updateSetOfColumns] = useUpdateSetOfColumnsMutation();
   const dispatch = useAppDispatch();
   const { columnsData } = useAppSelector((state) => state.board);
-  const [dataToRender, setDataToRender] = useState<ColumnType[]>([]);
+  const [columnsToRender, setColumnsToRender] = useState<ColumnType[]>([]);
 
   useEffect(() => {
     if (isError) {
       toast.error('Error');
     } else if (data) {
-      setDataToRender([...data].sort((a, b) => a.order - b.order));
+      setColumnsToRender([...data].sort((a, b) => a.order - b.order));
     }
   }, [data, isError]);
 
@@ -43,6 +43,7 @@ export const Columns = () => {
 
   const onDragEnd = async (result: DropResult) => {
     const { destination, source, type } = result;
+    console.log(destination, source, type);
 
     if (!destination) {
       return;
@@ -53,7 +54,7 @@ export const Columns = () => {
     }
 
     if (type === 'column') {
-      const updatedColumns = Array.from(dataToRender);
+      const updatedColumns = Array.from(columnsToRender);
       const movedColumn = updatedColumns.splice(source.index, 1);
       const columnsToUpdate: UpdateSetOfColumns[] = [];
 
@@ -65,7 +66,7 @@ export const Columns = () => {
         return { ...column, order: i };
       });
 
-      setDataToRender(updatedColumns);
+      setColumnsToRender(updatedColumns);
 
       await updateSetOfColumns(columnsToUpdate).unwrap();
 
@@ -76,21 +77,23 @@ export const Columns = () => {
     const endColumnId = destination.droppableId;
 
     if (startColumnId === endColumnId) {
-      const newTasks: TaskType[] = Array.from(columnsData[startColumnId]);
-      const movedTask = newTasks.splice(source.index, 1);
+      const tasks: TaskType[] = Array.from(columnsData[startColumnId]);
+      const movedTask = tasks.splice(source.index, 1);
 
-      newTasks.splice(destination.index, 0, ...movedTask);
-      newTasks.forEach((task, i) => (newTasks[i] = { ...task, order: i }));
+      tasks.splice(destination.index, 0, ...movedTask);
+      tasks.forEach((task, i) => (tasks[i] = { ...task, order: i }));
 
-      // console.log(columnsData[startColumnId]);
-      // console.log(newTasks);
+      dispatch(saveColumnTasks({ columnId: startColumnId, data: tasks }));
+
+      console.log(tasks);
+      console.log(columnsData[startColumnId]);
       //make request to update newTasks set
       return;
     }
 
-    const startTasks: TaskType[] = Array.from(columnsData[startColumnId]);
+    // const startTasks: TaskType[] = Array.from(columnsData[startColumnId]);
     // const movedTask = startTasks.splice(source.index, 1);
-    startTasks.forEach((task, i) => (startTasks[i] = { ...task, order: i }));
+    // startTasks.forEach((task, i) => (startTasks[i] = { ...task, order: i }));
 
     //get Tasks by endColumnId
     //finishTasks = Array.from(Tasks)
@@ -99,8 +102,8 @@ export const Columns = () => {
     //make request to update [startTasks, finishTasks] set
   };
 
-  const boardColumns = [...dataToRender].map((column, i) => (
-    <Column key={column._id} column={column} columns={dataToRender} boardIndex={i} />
+  const boardColumns = [...columnsToRender].map((column, i) => (
+    <Column key={column._id} column={column} columns={columnsToRender} boardIndex={i} />
   ));
 
   return (
