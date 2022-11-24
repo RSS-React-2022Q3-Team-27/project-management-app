33c3f0bfd37b385a885c2154344777c13ef50e5c
Bumble <bumble.sakh@gmail.com>
2022-11-25 04:29:04 +1100

    refactor: MainResults

--- src/components/Main/Boards/MainResults/MainResults.tsx
+++ src/components/Main/Boards/MainResults/MainResults.tsx
@@ -1,8 +1,10 @@
 import Box from '@mui/joy/Box';
 import CircularProgress from '@mui/joy/CircularProgress';
+import { useEffect } from 'react';
 
-import { useAppSelector } from '../../../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
 import { useGetBoardsByUserIdQuery } from '../../../../store/slices/boards/boardsApi';
+import { setBoards } from '../../../../store/slices/boards/boardsSlice';
 
 import { DialogEditBoard } from '../../../DialogEditBoard/DialogEditBoard';
 import { BoardCard } from '../BoardCard';
@@ -10,8 +12,15 @@ import { BoardCard } from '../BoardCard';
 export const MainResults = () => {
   const { isOpenedDialogEditBoard } = useAppSelector((state) => state.boards);
   const { id: userId } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
 
-  const { data: boards, isLoading } = useGetBoardsByUserIdQuery(userId);
+  const { data: boards, isLoading, isSuccess } = useGetBoardsByUserIdQuery(userId);
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setBoards(boards));
+    }
+  });
 
   const cards = boards?.map((board) => <BoardCard key={board._id} board={board} />);
 
